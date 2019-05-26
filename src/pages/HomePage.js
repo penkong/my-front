30a1809b48395ecca2987f6928a0f74175ea67dd
mazdak <nazemi.works@gmail.com>
2019-05-26 06:14:26 -0700

    problem with config of webpack

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const HomePage = () => (
   <div>
     <h1>Welcome</h1>
-    <p>This is my site. Take a look around!d   xxxxxxx</p>
+    <p>This is my site. Take a look around!dxxxxxxx</p>
   </div>
 );
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,36 @@
-const path = require('path');
+var webpack = require('webpack');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const VENDOR_LIBS = [
+  'axios',
+  'body-parser',
+  'cookie-session',
+  'express',
+  'history',
+  'normalize.css',
+  'react',
+  'react-dom',
+  'react-modal',
+  'react-redux',
+  'react-router-dom',
+  'redux',
+  'redux-form',
+  'redux-thunk',
+  'validator'
+];
+
+
+
 module.exports = {
-  entry: './src/client.js',
+  entry: {
+    bundle: './src/client.js',
+    vendor: VENDOR_LIBS //it produce vendor.js
+  },
   output: {
     path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
+    filename: '[name].[chunkhash].js'
   },
   module: {
     rules: [{
@@ -33,7 +59,7 @@ module.exports = {
     port: 3000,
     contentBase: path.join(__dirname, 'public'),
     historyApiFallback: true,
-    publicPath: '/dist/'
+    publicPath: '/public/'
   },
   plugins: [
     new MiniCssExtractPlugin({
@@ -42,5 +68,15 @@ module.exports = {
       filename: '[name].css',
       chunkFilename: '[id].css',
     }),
+    new webpack.optimization.splitChunks({
+      names: ['vendor', 'manifest'] //cause of hashing add it for browser ustand vendor change or not by manifest
+    }), //pull out duplicate and erase them.
+    new HtmlWebpackPlugin({ //we make new html in src and she use as template make new html in dist
+      template: 'src/index.html'
+    }), //produce new html with all new vendor and stuff
+    new webpack.DefinePlugin({ //this is for deploy
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }) //reactjs search for porcessenvnodeenv , dont do error checking so much in prod
+  //DEFINEPLUGIN make it available on window scope > add script of node to package 
   ]
 };
